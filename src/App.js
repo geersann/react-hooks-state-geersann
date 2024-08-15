ad8b608c321198c0f19ab4130138a76b0cab9e45
geersann <148795588+geersann@users.noreply.github.com>
2024-08-15 21:38:42 +0000

    add useState and handleLowerCase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import {useState} from 'react';
 import './App.css';
 
 export default function App() {
-  return <div> React Marathon </div>;
+  const [appData, setAppData] = useState('React Marathon');
+
+  const handleLowerCase = () => {
+    if(appData !== appData.toLowerCase()) {
+      return setAppData(appData.toLowerCase());
+    } else {
+      setAppData(prevData => {
+        return prevData
+        .split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+      })
+    }
+  }
+  return <div onClick={handleLowerCase}> {appData} </div>;
 }
 
